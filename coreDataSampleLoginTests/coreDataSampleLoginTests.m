8aa3b0d362927a06ae3b9e679256ebe47320aed4
Mani <mani@Manis-MacBook-Pro.local>
2017-08-07 23:04:56 +0530

    kook

--- coreDataSampleLoginTests/coreDataSampleLoginTests.m
+++ coreDataSampleLoginTests/coreDataSampleLoginTests.m
@@ -32,7 +32,7 @@
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+        // Put the code you want to measure the time of here...
     }];
 }
 
